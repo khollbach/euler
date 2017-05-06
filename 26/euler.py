da99982bc6559bab6f12269d2df6f94e75a239e5
Kevan Hollbach <khollbach@gmail.com>
2017-05-06 15:05:16 -0400

    26

--- 26/euler.py
+++ 26/euler.py
@@ -5,7 +5,46 @@ from utils import *
 import math
 
 def main():
-    pass
+    limit = 1000
+
+    d = None
+    max_length = 0
+
+    for n in range(2, limit):
+        decimals = []
+        seen_ms = []
+
+        # How long is the repeating string of digits? (if any)
+        length = 0
+
+        ## Long division.
+        # Exit once there is no remainder.
+        m = 1
+        while m != 0:
+            # Shift until n goes into m
+            while n > m:
+                m *= 10
+
+            # If we reach an m we've seen before, things will repeat.
+            if m in seen_ms:
+                # How long ago did we see this m?
+                length = len(seen_ms) - seen_ms.index(m)
+                break
+            seen_ms.append(m)
+
+            # How many times does n go into m?
+            decimals.append(m // n)
+
+            # Remainder?
+            m = m % n
+
+        if length > max_length:
+            d = n
+            max_length = length
+
+        print(n, length, decimals)
+
+    print(d, max_length)
 
 if __name__ == '__main__':
     import sys
