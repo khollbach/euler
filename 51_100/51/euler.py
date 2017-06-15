2ad60858de6360cff899179086fcfd4b78c1316e
Kevan Hollbach <khollbach@gmail.com>
2017-06-15 10:33:39 -0400

    Start 51

---
+++ 51_100/51/euler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from utils import *
+
+import math
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    import sys
+    sys.exit(main())
