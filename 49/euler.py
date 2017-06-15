5c6bcd275e8f88ebf9029b743d5440c1e7f68386
Kevan Hollbach <khollbach@gmail.com>
2017-06-15 09:36:01 -0400

    49

--- 49/euler.py
+++ 49/euler.py
@@ -11,7 +11,17 @@ def main():
         if is_prime(i):
             is_prime_fast[i] = True
 
-    # TODO
+    for i in range(1000, 10000):
+        if is_prime_fast[i]:
+            for j in range(i+1, 10000):
+                k = j + (j - i)
+                if k < 10000 and is_prime_fast[j] and is_prime_fast[k] \
+                    and nums_are_perms(i, j) and nums_are_perms(j, k):
+
+                    print(i, j, k)
+
+def nums_are_perms(n1, n2):
+    return sorted(str(n1)) == sorted(str(n2))
 
 if __name__ == '__main__':
     import doctest
